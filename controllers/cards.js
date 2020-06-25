0636dfed23cee2b73c71307bcf51653f8f12dfb5
Danil Plyusnin <dplusnin0@gmail.com>
2020-06-25 17:03:40 +0600

    First commit

---
+++ controllers/cards.js
@@ -0,0 +1,58 @@
+const Card = require('../models/card');
+const NotFoundError = require('../errors/notFoundError');
+
+module.exports.getCards = (req, res) => {
+  Card.find({})
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: 'Произошла ошибка', err }));
+};
+
+module.exports.createCard = (req, res, next) => {
+  const { name, link } = req.body;
+  console.log(req.user.id);
+  console.log(req.body);
+  Card.create({ name, link, owner: req.user._id })
+    .then((card) => res.send({ data: card }))
+    .catch((err) => {
+      if (err) {
+        res.status(500).send({ message: 'Произошла ошибка', err });
+      }
+      next(err);
+    });
+};
+
+
+module.exports.deleteCard = (req, res) => {
+  Card.findById(req.params.id)
+    .then((card) => {
+      if (!card) {
+        return Promise.reject(new Error(`Карточка с _id:${req.params.id} не найдена в базе данных`));
+      }
+      const { owner } = card;
+      return owner;
+    })
+    .then((owner) => {
+      if (req.user._id === owner.toString()) {
+        return Card.findByIdAndRemove(req.params.id);
+      }
+      return Promise.reject(new Error('нет доступа для удаления карточки'));
+    })
+    .then(() => res.status(200).send({ message: `Карточка с _id:${req.params.id} успешно удалена из базы данных` }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+
+module.exports.likeCard = (req, res, next) => {
+  Card.findByIdAndUpdate(req.params.id, { $addToSet: { likes: req.user._id } }, { new: true })
+    .orFail(() => new NotFoundError('Карточка не найдена'))
+    .then((card) => res.send({ data: card }))
+    .catch(next);
+};
+
+
+module.exports.dislikeCard = (req, res, next) => {
+  Card.findByIdAndUpdate(req.params.id, { $pull: { likes: req.user._id } }, { new: true })
+    .orFail(() => new NotFoundError('Карточка не найдена'))
+    .then((card) => res.send({ data: card }))
+    .catch(next);
+};
---
+++ controllers/users.js
@@ -0,0 +1,83 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const NotFoundError = require('../errors/notFoundError');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+module.exports.getUsers = (req, res) => {
+  User.find({})
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: 'Произошла ошибка', err }));
+};
+
+module.exports.getUserById = (req, res, next) => {
+  User.findById(req.params.id)
+    .orFail(() => new NotFoundError('Карточка не найдена'))
+    .then((user) => res.send({ data: user }))
+    .catch(next);
+};
+
+module.exports.createUser = (req, res) => {
+  const {
+    name,
+    about,
+    avatar,
+    email,
+  } = req.body;
+  console.log(req.body);
+  bcrypt.hash(req.body.password, 10)
+    .then((hash) => User.create({
+      name,
+      about,
+      avatar,
+      email,
+      password: hash,
+    }))
+
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(400).send({ message: err.message }));
+};
+
+// обновление профиля пользователя
+module.exports.updateUserProfile = (req, res) => {
+  const { name, about } = req.body;
+  console.log(req.body);
+  User.findByIdAndUpdate(req.user._id, { name, about }, {
+    new: true, // обработчик then получит на вход обновлённую запись
+    runValidators: true, // данные будут валидированы перед изменением
+    upsert: false, // если пользователь не найден, он не будет создан
+  })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: 'Произошла ошибка', err }));
+};
+
+// обновление аватара пользователя
+module.exports.updateUserAvatar = (req, res) => {
+  const { avatar } = req.body;
+  console.log(req.body);
+  User.findByIdAndUpdate(req.user._id, { avatar }, {
+    new: true, // обработчик then получит на вход обновлённую запись
+    runValidators: true, // данные будут валидированы перед изменением
+    upsert: false, // если пользователь не найден, он не будет создан
+  })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: 'Произошла ошибка', err }));
+};
+
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+  return User.findUser(email, password)
+    .then((user) => {
+      // const token = jwt.sign({ _id: user._id }, 'secret-key', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
+      res.send({ token });
+      res.cookie('jwt', token, {
+        maxAge: 3600000 * 24 * 7,
+        httpOnly: true,
+      });
+    })
+    .catch((err) => {
+      res.status(401).send({ message: err.message });
+    });
+};
